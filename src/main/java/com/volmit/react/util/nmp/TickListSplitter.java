8dc88f2cc49d3d50b07542ed768615f4cee3fe81
cyberpwn <danielmillst@gmail.com>
2019-01-30 21:29:30 -0500

    Splitter support fluids for 1.13

--- src/main/java/com/volmit/react/util/nmp/TickListSplitter.java
+++ src/main/java/com/volmit/react/util/nmp/TickListSplitter.java
@@ -8,13 +8,16 @@ import org.bukkit.block.Block;
 
 import com.volmit.volume.lang.collections.GList;
 import com.volmit.volume.lang.collections.GMap;
+import com.volmit.volume.math.M;
 
 public class TickListSplitter
 {
 	private final World world;
 	private final Set<Object> master;
+	private final Set<Object> masterFluid;
 	private final CatalystHost host;
 	private final GMap<Object, Integer> withold;
+	private final GMap<Object, Integer> witholdFluid;
 	private final GMap<Material, Integer> witholdTypes;
 	private int globalThrottle;
 
@@ -23,9 +26,32 @@ public class TickListSplitter
 		this.world = world;
 		host = Catalyst.host;
 		master = host.getTickList(world);
+		masterFluid = host.getTickListFluid(world);
 		witholdTypes = new GMap<>();
 		withold = new GMap<>();
+		witholdFluid = new GMap<>();
 		globalThrottle = 0;
+		setGlobalThrottle(5);
+	}
+
+	public int getTickCount()
+	{
+		return master.size() + masterFluid.size();
+	}
+
+	public int getWitheldCount()
+	{
+		return withold.size() + witholdFluid.size();
+	}
+
+	public void setPhysicsSpeed(double d)
+	{
+		setGlobalThrottle((int) (M.clip(d, 0, 1D) * 20D));
+	}
+
+	public double getPhysicsSpeed()
+	{
+		return 1D - (double) M.clip(globalThrottle, 0, 20D) / 20D;
 	}
 
 	public void unregisterAll()
@@ -55,7 +81,7 @@ public class TickListSplitter
 
 	public void dumpAll()
 	{
-		while(!withold.isEmpty())
+		while(getWitheldCount() > 0)
 		{
 			dumpWitheldTickList();
 		}
@@ -81,6 +107,24 @@ public class TickListSplitter
 			}
 		}
 
+		for(Object i : new GList<>(masterFluid))
+		{
+			Block b = host.getBlock(world, i);
+			Material t = b.getType();
+
+			if(witholdTypes.containsKey(t))
+			{
+				withold.put(i, witholdTypes.get(t));
+				masterFluid.remove(i);
+			}
+
+			else if(globalThrottle > 0)
+			{
+				withold.put(i, globalThrottle);
+				masterFluid.remove(i);
+			}
+		}
+
 		dumpWitheldTickList();
 	}
 
@@ -94,6 +138,17 @@ public class TickListSplitter
 			{
 				withold.remove(i);
 				master.add(i);
+			}
+		}
+
+		for(Object i : witholdFluid.k())
+		{
+			witholdFluid.put(i, witholdFluid.get(i) - 1);
+
+			if(witholdFluid.get(i) <= 0)
+			{
+				witholdFluid.remove(i);
+				masterFluid.add(i);
 			}
 		}
 	}
