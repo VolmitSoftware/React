5e7148386bbd45638138878c16d14717ea91cd6f
cyberpwn <danielmillst@gmail.com>
2019-02-01 08:56:20 -0500

    Fix action issues

--- src/main/java/com/volmit/react/api/ActionType.java
+++ src/main/java/com/volmit/react/api/ActionType.java
@@ -5,11 +5,11 @@ import com.volmit.react.Info;
 public enum ActionType
 {
 	FIX_LIGHTING(ActionHandle.MUTEX, ActionTargetType.POSITIONAL, Info.ACTION_FIX_LIGHTING_NAME, Info.ACTION_FIX_LIGHTING_DESCRIPTION),
+	THROTTLE_PHYSICS(ActionHandle.MUTEX, ActionTargetType.POSITIONAL, "Throttle Physics", "Slows down the tickrate in the selected area."),
 	CPU_SCORE(ActionHandle.MUTEX, ActionTargetType.SYSTEM, "CPU Score", "Tempts the processor to turbo and catches the score."),
 	DUMP(ActionHandle.MUTEX, ActionTargetType.SYSTEM, "Dump", "Dumps debugging and performance information into paste."),
 	FILE_SIZE(ActionHandle.MUTEX, ActionTargetType.SYSTEM, "File Size", "Checks the server files for a size report."),
 	TIMINGS(ActionHandle.MUTEX, ActionTargetType.SYSTEM, "Timings Report", "Pulls a timings report for some time."),
-	CHUNK_TEST(ActionHandle.MUTEX, ActionTargetType.POSITIONAL, Info.ACTION_CHUNK_TEST_NAME, Info.ACTION_CHUNK_TEST_DESCRIPTION),
 	CULL_ENTITIES(ActionHandle.MUTEX, ActionTargetType.POSITIONAL, Info.ACTION_CULL_ENTITIES_NAME, Info.ACTION_CULL_ENTITIES_DESCRIPTION),
 	COLLECT_GARBAGE(ActionHandle.MUTEX, ActionTargetType.SYSTEM, Info.ACTION_COLLECT_GARBAGE_NAME, Info.ACTION_COLLECT_GARBAGE_DESCRIPTION),
 	PURGE_ENTITIES(ActionHandle.MUTEX, ActionTargetType.POSITIONAL, Info.ACTION_PURGE_ENTITIES_NAME, Info.ACTION_PURGE_ENTITIES_DESCRIPTION),
