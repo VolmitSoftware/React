b0d7ff766c636a187ae4568b8998963635f722ed
Andrew Baker <andrew@andrewcbaker.net>
2021-02-02 09:15:36 -0800

    Fix map
    Took 6 minutes

--- src/main/java/com/volmit/react/api/ReactFont.java
+++ src/main/java/com/volmit/react/api/ReactFont.java
@@ -4,10 +4,6 @@ import org.bukkit.map.MapFont;
 
 public class ReactFont extends MapFont {
 
-    /**
-     * A static non-malleable MinecraftFont.
-     */
-    public static final ReactFont Font = new ReactFont(false);
     private static final int spaceSize = 2;
     private static final String fontChars = " !\"#$%&'()*+,-./0123456789:;<=>?" + "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_" + "'abcdefghijklmnopqrstuvwxyz{|}~\u007F" + "\u00C7\u00FC\u00E9\u00E2\u00E4\u00E0\u00E5\u00E7" + "\u00EA\u00EB\u00E8\u00EF\u00EE\u00EC\u00C4\u00C5" + "\u00C9\u00E6\u00C6\u00F4\u00F6\u00F2\u00FB\u00F9" + "\u00FF\u00D6\u00DC\u00F8\u00A3\u00D8\u00D7\u0191" + "\u00E1\u00ED\u00F3\u00FA\u00F1\u00D1\u00AA\u00BA" + "\u00BF\u00AE\u00AC\u00BD\u00BC\u00A1\u00AB\u00BB";
     private static final int[][] fontData = new int[][]{
@@ -270,6 +266,11 @@ public class ReactFont extends MapFont {
             {0, 0, 0, 0, 0, 0, 0, 0},
             //@fuckboy:off
     };
+
+    /**
+     * A static non-malleable MinecraftFont.
+     */
+    public static final ReactFont Font = new ReactFont(false);
 
     /**
      * Initialize a new MinecraftFont.
