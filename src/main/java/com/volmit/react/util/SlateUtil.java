e3b8a8ac906d2574edebaaf96beaade5c4738a96
Andrew Baker <andrew@andrewcbaker.net>
2021-02-16 00:16:40 -0800

    🦀 Fix scoreboard flickering 🦀

--- src/main/java/com/volmit/react/util/SlateUtil.java
+++ src/main/java/com/volmit/react/util/SlateUtil.java
@@ -1,12 +1,9 @@
 package com.volmit.react.util;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
-import org.bukkit.scoreboard.DisplaySlot;
-import org.bukkit.scoreboard.Objective;
-import org.bukkit.scoreboard.Scoreboard;
-import org.bukkit.scoreboard.ScoreboardManager;
-
+import org.bukkit.scoreboard.*;
 import primal.lang.collection.GList;
 
 /**
@@ -14,209 +11,200 @@ import primal.lang.collection.GList;
  *
  * @author cyberpwn
  */
-public class SlateUtil
-{
-	/**
-	 * Get the board manager
-	 *
-	 * @return the manager
-	 */
-	public static ScoreboardManager getManager()
-	{
-		return Bukkit.getScoreboardManager();
-	}
+public class SlateUtil {
+    /**
+     * Get the board manager
+     *
+     * @return the manager
+     */
+    public static ScoreboardManager getManager() {
+        return Bukkit.getScoreboardManager();
+    }
 
-	/**
-	 * Create a new board
-	 *
-	 * @return the board
-	 */
-	public static Scoreboard newBoard()
-	{
-		return getManager().getNewScoreboard();
-	}
+    /**
+     * Create a new board
+     *
+     * @return the board
+     */
+    public static Scoreboard newBoard() {
+        return getManager().getNewScoreboard();
+    }
 
-	/**
-	 * Create a new objective
-	 *
-	 * @param board
-	 *            the binding board
-	 * @param name
-	 *            the name of the objective
-	 * @return the objective
-	 */
-	public static Objective newObjective(Scoreboard board, String name)
-	{
-		Objective o = board.registerNewObjective("slate", "dummy");
+    /**
+     * Create a new objective
+     *
+     * @param board the binding board
+     * @param name  the name of the objective
+     * @return the objective
+     */
+    public static Objective newObjective(Scoreboard board, String name) {
+        Objective o = board.registerNewObjective("slate", "dummy");
 
-		if(name.length() > 32)
-		{
-			name = name.substring(0, 29) + "...";
-		}
+        if (name.length() > 32) {
+            name = name.substring(0, 29) + "...";
+        }
 
-		o.setDisplayName(name);
-		o.setDisplaySlot(DisplaySlot.SIDEBAR);
+        o.setDisplayName(name);
+        o.setDisplaySlot(DisplaySlot.SIDEBAR);
 
-		return o;
-	}
+        return o;
+    }
 
-	public static Objective newHeadObjective(Scoreboard board, String name)
-	{
-		Objective o = board.registerNewObjective("slate", "dummy");
+    public static Objective newHeadObjective(Scoreboard board, String name) {
+        Objective o = board.registerNewObjective("slate", "dummy");
 
-		if(name.length() > 32)
-		{
-			name = name.substring(0, 29) + "...";
-		}
+        if (name.length() > 32) {
+            name = name.substring(0, 29) + "...";
+        }
 
-		o.setDisplayName(name);
-		o.setDisplaySlot(DisplaySlot.BELOW_NAME);
+        o.setDisplayName(name);
+        o.setDisplaySlot(DisplaySlot.BELOW_NAME);
 
-		return o;
-	}
+        return o;
+    }
 
-	/**
-	 * Create a new objective
-	 *
-	 * @param board
-	 *            the binding board
-	 * @param name
-	 *            the name of the objective
-	 * @return the objective
-	 */
-	public static Objective newTabObjective(Scoreboard board, String name)
-	{
-		Objective o = board.registerNewObjective("slate", "dummy");
+    /**
+     * Create a new objective
+     *
+     * @param board the binding board
+     * @param name  the name of the objective
+     * @return the objective
+     */
+    public static Objective newTabObjective(Scoreboard board, String name) {
+        Objective o = board.registerNewObjective("slate", "dummy");
 
-		if(name.length() > 32)
-		{
-			name = name.substring(0, 29) + "...";
-		}
+        if (name.length() > 32) {
+            name = name.substring(0, 29) + "...";
+        }
 
-		o.setDisplayName(name);
-		o.setDisplaySlot(DisplaySlot.PLAYER_LIST);
+        o.setDisplayName(name);
+        o.setDisplaySlot(DisplaySlot.PLAYER_LIST);
 
-		return o;
-	}
+        return o;
+    }
 
-	/**
-	 * Set the score with text
-	 *
-	 * @param name
-	 *            the name
-	 * @param value
-	 *            the value
-	 * @param o
-	 *            the objective
-	 */
-	public static void setScore(String name, int value, Objective o)
-	{
-		if(Protocol.R1_7_1.to(Protocol.R1_7_10).contains(Protocol.getProtocolVersion()))
-		{
-			if(name.length() > 15)
-			{
-				name = name.substring(0, 15);
-			}
-		}
+    /**
+     * Set the score with text
+     *
+     * @param name  the name
+     * @param value the value
+     * @param o     the objective
+     */
+    public static void setScore(String name, int value, Objective o) {
+        if (Protocol.R1_7_1.to(Protocol.R1_7_10).contains(Protocol.getProtocolVersion())) {
+            if (name.length() > 15) {
+                name = name.substring(0, 15);
+            }
+        }
 
-		if(name.length() > 40)
-		{
-			name = name.substring(0, 37) + "...";
-		}
+        if (name.length() > 40) {
+            name = name.substring(0, 37) + "...";
+        }
 
-		o.getScore(name).setScore(value);
-	}
+        o.getScore(name).setScore(value);
+    }
 
-	/**
-	 * Build a custom slate
-	 *
-	 * @param name
-	 *            the name of it
-	 * @param data
-	 *            the list of data
-	 * @return the slate
-	 */
-	public static Scoreboard buildSlate(String name, GList<String> data)
-	{
-		Scoreboard slate = newBoard();
-		Objective o = newObjective(slate, name);
+    /**
+     * Build a custom slate
+     *
+     * @param name the name of it
+     * @param data the list of data
+     * @return the slate
+     */
+    public static Scoreboard buildSlate(String name, GList<String> data) {
+        Scoreboard slate = newBoard();
+        Objective o = newObjective(slate, name);
 
-		int ind = data.size();
+        int ind = data.size();
+        for (String i : data) {
+            try {
+                Team team = slate.registerNewTeam("team" + ind);
+                team.addEntry(ChatColor.values()[ind] + "");
+                team.setPrefix(i);
+                setScore(ChatColor.values()[ind] + "", ind, o);
 
-		for(String i : data)
-		{
-			setScore(i, ind, o);
+                ind--;
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+        }
 
-			ind--;
-		}
+        return slate;
+    }
 
-		return slate;
-	}
+    /**
+     * Update a slate
+     *
+     * @param slate the slate to update
+     * @param data  the list of data
+     * @return the slate
+     */
+    public static Scoreboard updateSlate(Scoreboard slate, GList<String> data) {
+        int ind = data.size();
+        for (String i : data) {
+            try {
+                slate.getTeam("team" + ind).setPrefix(i);
+                ind--;
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+        }
 
-	/**
-	 * Build a custom slate
-	 *
-	 * @param name
-	 *            the name of it
-	 * @param data
-	 *            the list of data
-	 * @return the slate
-	 */
-	public static Scoreboard buildTabSlate(String name, GList<String> data)
-	{
-		Scoreboard slate = newBoard();
-		Objective o = newTabObjective(slate, name);
+        return slate;
+    }
 
-		int ind = data.size();
+    /**
+     * Build a custom slate
+     *
+     * @param name the name of it
+     * @param data the list of data
+     * @return the slate
+     */
+    public static Scoreboard buildTabSlate(String name, GList<String> data) {
+        Scoreboard slate = newBoard();
+        Objective o = newTabObjective(slate, name);
 
-		for(String i : data)
-		{
-			setScore(i, ind, o);
+        int ind = data.size();
 
-			ind--;
-		}
+        for (String i : data) {
+            setScore(i, ind, o);
 
-		return slate;
-	}
+            ind--;
+        }
 
-	public static String convertJSON(String noJson)
-	{
-		if(noJson == null)
-		{
-			return null;
-		}
+        return slate;
+    }
 
-		if(noJson.startsWith("{") && noJson.endsWith("}"))
-		{
-			return noJson;
-		}
+    public static String convertJSON(String noJson) {
+        if (noJson == null) {
+            return null;
+        }
 
-		return String.format("{\"text\":\"%s\"}", noJson);
-	}
+        if (noJson.startsWith("{") && noJson.endsWith("}")) {
+            return noJson;
+        }
 
-	public static String[] convertJSON(String... noJson)
-	{
-		if(noJson == null)
-		{
-			return null;
-		}
+        return String.format("{\"text\":\"%s\"}", noJson);
+    }
 
-		String[] converted = new String[noJson.length];
+    public static String[] convertJSON(String... noJson) {
+        if (noJson == null) {
+            return null;
+        }
 
-		for(int i = 0; i < noJson.length; i++)
-		{
-			if(i != 0 && (!noJson[i].startsWith("{") || !noJson[i].endsWith("}")))
-			{
-				noJson[i] = "\\n" + noJson[i];
-			}
-			converted[i] = convertJSON(noJson[i]);
-		}
+        String[] converted = new String[noJson.length];
 
-		return converted;
-	}
+        for (int i = 0; i < noJson.length; i++) {
+            if (i != 0 && (!noJson[i].startsWith("{") || !noJson[i].endsWith("}"))) {
+                noJson[i] = "\\n" + noJson[i];
+            }
+            converted[i] = convertJSON(noJson[i]);
+        }
 
-	public static void setTabTitle(Player p, String header, String footer)
-	{
-		NMSX.sendTabTitle(p, header, footer);
-	}
+        return converted;
+    }
+
+    public static void setTabTitle(Player p, String header, String footer) {
+        NMSX.sendTabTitle(p, header, footer);
+    }
 }
