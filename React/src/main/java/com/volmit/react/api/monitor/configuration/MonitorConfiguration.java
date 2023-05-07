ee6f1a8731f07a6cd2784a5f2afb882e1b46529c
cyberpwn <danielmillst@gmail.com>
2023-05-07 18:59:08 -0400

    Move to subfolder

---
+++ React/src/main/java/com/volmit/react/api/monitor/configuration/MonitorConfiguration.java
@@ -0,0 +1,24 @@
+package com.volmit.react.api.monitor.configuration;
+
+import com.volmit.react.React;
+import com.volmit.react.api.sampler.Sampler;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Singular;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+@Builder
+public class MonitorConfiguration {
+    @Singular
+    private List<MonitorGroup> groups;
+
+    public List<Sampler> getAllSamplers() {
+        return groups.stream()
+                .flatMap(group -> group.getSamplers().stream())
+                .map(i -> React.instance.getSampleController().getSampler(i))
+                .collect(Collectors.toList());
+    }
+}
---
+++ React/src/main/java/com/volmit/react/api/sampler/ReactCachedSampler.java
@@ -0,0 +1,44 @@
+package com.volmit.react.api.sampler;
+
+import com.google.common.util.concurrent.AtomicDouble;
+import com.volmit.react.legacyutil.ChronoLatch;
+
+public abstract class ReactCachedSampler implements Sampler {
+    private final ChronoLatch latch;
+    private final AtomicDouble last;
+    private final String id;
+
+    public ReactCachedSampler(String id, long sampleDelay) {
+        this.id = id;
+        this.latch = new ChronoLatch(sampleDelay, true);
+        this.last = new AtomicDouble();
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    public abstract double onSample();
+
+    @Override
+    public double sample() {
+        double t = last.get();
+
+        if (latch.flip()) {
+            t = onSample();
+            last.set(t);
+        }
+
+        return t;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
