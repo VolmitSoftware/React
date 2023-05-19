90d70c98ce614d3b2b4b4f512a3a52da4b74dfa3
cyberpwn <danielmillst@gmail.com>
2023-05-19 12:55:33 -0400

    typo

--- React/src/main/java/com/volmit/react/content/command/CommandReact.java
+++ React/src/main/java/com/volmit/react/content/command/CommandReact.java
@@ -6,10 +6,6 @@ import com.volmit.react.util.decree.DecreeOrigin;
 import com.volmit.react.util.decree.annotations.Decree;
 import com.volmit.react.util.format.C;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Decree(
         name = "react",
         aliases = {"re"},
@@ -20,7 +16,7 @@ public class CommandReact implements DecreeExecutor {
     private CommandConfig config;
     private CommandAction action;
     private CommandChunk chunk;
-    private CommandEnvironment enviornment;
+    private CommandEnvironment environment;
 
     @Decree(
         name = "monitor",
@@ -31,23 +27,5 @@ public class CommandReact implements DecreeExecutor {
     public void monitor() {
         React.instance.getPlayerController().getPlayer(player()).toggleActionBar();
         sender().sendMessage(C.REACT + "Action bar monitor " + (React.instance.getPlayerController().getPlayer(player()).isActionBarMonitoring() ? "enabled" : "disabled"));
-    }
-
-    public static final List<Material> all = Arrays.stream(Material.values()).filter((m) -> m.getMaxStackSize() == 64 && !m.isLegacy()).collect(Collectors.toList());
-
-    @Decree(
-        name = "all",
-        description = "Give random items",
-        origin = DecreeOrigin.PLAYER
-    )
-    public void all() {
-
-        for(int i = 0; i < 10; i++)
-        {
-
-
-player().getInventory().addItem(new ItemStack(all.get(RNG.r.i(all.size()-1)), RNG.r.i(1,64)));
-
-        }
     }
 }
