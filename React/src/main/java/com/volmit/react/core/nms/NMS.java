305134b353370e0228e38c09716c2f9310cbd101
cyberpwn <danielmillst@gmail.com>
2023-05-21 15:51:34 -0400

    Fixes

--- React/src/main/java/com/volmit/react/core/nms/NMS.java
+++ React/src/main/java/com/volmit/react/core/nms/NMS.java
@@ -4,6 +4,7 @@ import art.arcane.curse.Curse;
 import art.arcane.curse.model.CursedComponent;
 import art.arcane.curse.model.FuzzyMethod;
 import com.volmit.react.React;
+import com.volmit.react.content.feature.FeatureCircuitManager;
 import com.volmit.react.util.data.B;
 import com.volmit.react.util.scheduling.J;
 import it.unimi.dsi.fastutil.ints.IntArrayList;
@@ -11,9 +12,6 @@ import it.unimi.dsi.fastutil.ints.IntList;
 import net.minecraft.network.protocol.game.PacketPlayOutEntityDestroy;
 import net.minecraft.network.protocol.game.PacketPlayOutEntityMetadata;
 import net.minecraft.network.syncher.DataWatcher;
-import net.minecraft.network.syncher.DataWatcherRegistry;
-import net.minecraft.server.level.EntityPlayer;
-import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.block.Block;
@@ -36,6 +34,18 @@ public interface NMS {
 
     default CursedComponent curse(String className) {
         return Curse.on(classFor(className));
+    }
+
+    List<?> getDataWatcherProperties(Object... objects);
+
+    @Flimsy
+    default Object getByteDataWatcherType() {
+        return curse("net.minecraft.network.syncher.DataWatcherRegistry").get("a");
+    }
+
+    @Flimsy
+    default Object getBooleanDataWatcherType() {
+        return curse("net.minecraft.network.syncher.DataWatcherRegistry").get("j");
     }
 
     default Class<?> classFor(String canonical) {
@@ -62,11 +72,22 @@ public interface NMS {
             .construct(new IntArrayList(entity)).instance();
     }
 
-    Object getEntityMetadataPacket(int eid, List<Object> items);
+    default Object getEntityMetadataPacket(int eid, List<?> items) {
+        return curse("net.minecraft.network.protocol.game.PacketPlayOutEntityMetadata")
+            .construct(eid, items).instance();
+    }
 
-    Object getGravityProperty(boolean gravity);
+    @Flimsy
+    default Object getEntityStateProperty(byte state) {
+        return curse("net.minecraft.network.syncher.DataWatcher.DataWatcher.b")
+            .construct(0, getByteDataWatcherType(), state).instance();
+    }
 
-    Object getEntityStateProperty(byte state);
+    @Flimsy
+    default Object getGravityProperty(boolean gravity) {
+        return curse("net.minecraft.network.syncher.DataWatcher.DataWatcher.b")
+            .construct(5, getBooleanDataWatcherType(), !gravity).instance();
+    }
 
     default void sendPacket(Block at, int radius, Object packet) {
         sendPacket(at.getLocation(), radius, packet);
--- React/src/main/java/com/volmit/react/core/nms/providers/R19.java
+++ React/src/main/java/com/volmit/react/core/nms/providers/R19.java
@@ -1,6 +1,7 @@
 package com.volmit.react.core.nms.providers;
 
 import com.volmit.react.core.nms.NMS;
+import net.minecraft.network.protocol.game.PacketPlayOutEntityMetadata;
 import net.minecraft.network.syncher.DataWatcher;
 import net.minecraft.network.syncher.DataWatcherRegistry;
 
@@ -8,17 +9,23 @@ import java.util.List;
 
 public class R19 implements NMS {
     @Override
-    public Object getEntityMetadataPacket(int eid, List<Object> items) {
-        return null;
-    }
+    public List<?> getDataWatcherProperties(Object... objects) {
+        List<DataWatcher.b<?>> list = new java.util.ArrayList<>();
 
-    @Override
-    public Object getGravityProperty(boolean gravity) {
-        return null;
+        for(Object i : objects) {
+            list.add((DataWatcher.b<?>) i);
+        }
+
+        return list;
     }
 
     @Override
     public Object getEntityStateProperty(byte state) {
         return new DataWatcher.b<>(0, DataWatcherRegistry.a, state);
+    }
+
+    @Override
+    public Object getGravityProperty(boolean gravity) {
+        return new DataWatcher.b<>(5, DataWatcherRegistry.j, !gravity);
     }
 }
