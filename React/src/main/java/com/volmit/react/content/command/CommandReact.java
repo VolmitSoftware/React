06e251827094dc2c27e63ded31b25bf6d1d7f922
RePixelatedMC <107539181+RePixelatedMC@users.noreply.github.com>
2024-04-04 14:21:46 +0200

    Sync + redid /react env info

--- React/src/main/java/com/volmit/react/content/command/CommandReact.java
+++ React/src/main/java/com/volmit/react/content/command/CommandReact.java
@@ -25,12 +25,16 @@ import com.volmit.react.React;
 import com.volmit.react.api.benchmark.CPUBenchmark;
 import com.volmit.react.api.benchmark.Hastebin;
 import com.volmit.react.api.benchmark.MemoryBenchmark;
+import com.volmit.react.util.collection.KList;
 import com.volmit.react.util.format.C;
 import com.volmit.react.util.format.Form;
+import com.volmit.react.util.hardware.getHardware;
 import com.volmit.react.util.reflect.Platform;
 import com.volmit.react.util.scheduling.J;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.checkerframework.checker.units.qual.K;
+import oshi.SystemInfo;
 
 //THIS IS A DUMMY CLASS FOR EDICT WHENEVER ITS DONE use @decree instead of @edict
 public class CommandReact {
@@ -63,22 +67,42 @@ public class CommandReact {
     @Edict.Command("/react environment")
     @Edict.Aliases("/react env")
     public static void environment() {
+        SystemInfo systemInfo = new SystemInfo();
+        KList<String> disks = new KList<>(getHardware.getDisk());
+        KList<String> interfaces = new KList<>(getHardware.getInterfaces());
+        KList<String> displays = new KList<>(getHardware.getEDID());
+        KList<String> powersources = new KList<>(getHardware.getPowerSources());
+
+
         EdictContext.sender().sendMessage(String.valueOf(C.BOLD) + C.DARK_AQUA + " -- == React Info == -- ");
-        EdictContext.sender().sendMessage(C.GOLD + "React Version Version: " + React.instance.getDescription().getVersion());
-        EdictContext.sender().sendMessage(C.GOLD + "Server Type: " + Bukkit.getVersion());
+        EdictContext.sender().sendMessage(C.AQUA + "- React Version Version: " + React.instance.getDescription().getVersion());
+        EdictContext.sender().sendMessage(C.AQUA + "- Server Type: " + Bukkit.getVersion());
         EdictContext.sender().sendMessage(String.valueOf(C.BOLD) + C.DARK_AQUA + " -- == Platform Overview == -- ");
-        EdictContext.sender().sendMessage(C.GOLD + "Version: " + Platform.getVersion() + " - Platform: " + Platform.getName());
-        EdictContext.sender().sendMessage(C.GOLD + "Java Vendor: " + Platform.ENVIRONMENT.getJavaVendor() + " - Java Version: " + Platform.ENVIRONMENT.getJavaVersion());
+        EdictContext.sender().sendMessage(C.AQUA + "- Version: " + Platform.getVersion() + " - Platform: " + Platform.getName());
+        EdictContext.sender().sendMessage(C.AQUA + "- Java Vendor: " + Platform.ENVIRONMENT.getJavaVendor() + " - Java Version: " + Platform.ENVIRONMENT.getJavaVersion());
         EdictContext.sender().sendMessage(String.valueOf(C.BOLD) + C.DARK_AQUA + " -- == Storage Information == -- ");
-        EdictContext.sender().sendMessage(C.GOLD + "Total Space: " + Form.memSize(Platform.STORAGE.getTotalSpace()));
-        EdictContext.sender().sendMessage(C.GOLD + "Free Space: " + Form.memSize(Platform.STORAGE.getFreeSpace()));
-        EdictContext.sender().sendMessage(C.GOLD + "Used Space: " + Form.memSize(Platform.STORAGE.getUsedSpace()));
+        for (String disk : disks) {
+            EdictContext.sender().sendMessage(C.AQUA + "- " + disk);
+        }
+        EdictContext.sender().sendMessage(String.valueOf(C.BOLD) + C.DARK_AQUA + " -- == Interface Information == -- ");
+        for (String inter : interfaces) {
+            EdictContext.sender().sendMessage(C.AQUA + "- " + inter);
+        }
+        EdictContext.sender().sendMessage(String.valueOf(C.BOLD) + C.DARK_AQUA + " -- == Display Information == -- ");
+        for (String display : displays) {
+            EdictContext.sender().sendMessage(C.AQUA + "- " + display);
+        }
+        EdictContext.sender().sendMessage(String.valueOf(C.BOLD) + C.DARK_AQUA + " -- == Power Information == -- ");
+        for (String power : powersources) {
+            EdictContext.sender().sendMessage(C.AQUA + "- " + power);
+        }
         EdictContext.sender().sendMessage(String.valueOf(C.BOLD) + C.DARK_AQUA + " -- == Memory Information == -- ");
-        EdictContext.sender().sendMessage(C.GOLD + "Physical Memory - Total: " + Form.memSize(Platform.MEMORY.PHYSICAL.getTotalMemory()) + " Free: " + Form.memSize(Platform.MEMORY.PHYSICAL.getFreeMemory()) + " Used: " + Form.memSize(Platform.MEMORY.PHYSICAL.getUsedMemory()));
-        EdictContext.sender().sendMessage(C.GOLD + "Virtual Memory - Total: " + Form.memSize(Platform.MEMORY.VIRTUAL.getTotalMemory()) + " Free: " + Form.memSize(Platform.MEMORY.VIRTUAL.getFreeMemory()) + " Used: " + Form.memSize(Platform.MEMORY.VIRTUAL.getUsedMemory()));
+        EdictContext.sender().sendMessage(C.AQUA + "- Physical Memory - Total: " + Form.memSize(Platform.MEMORY.PHYSICAL.getTotalMemory()) + " Free: " + Form.memSize(Platform.MEMORY.PHYSICAL.getFreeMemory()) + " Used: " + Form.memSize(Platform.MEMORY.PHYSICAL.getUsedMemory()));
+        EdictContext.sender().sendMessage(C.AQUA + "- Virtual Memory - Total: " + Form.memSize(Platform.MEMORY.VIRTUAL.getTotalMemory()) + " Free: " + Form.memSize(Platform.MEMORY.VIRTUAL.getFreeMemory()) + " Used: " + Form.memSize(Platform.MEMORY.VIRTUAL.getUsedMemory()));
         EdictContext.sender().sendMessage(String.valueOf(C.BOLD) + C.DARK_AQUA + " -- == CPU Overview == -- ");
-        EdictContext.sender().sendMessage(C.GOLD + "CPU Architecture: " + Platform.CPU.getArchitecture() + " Available Processors: " + Platform.CPU.getAvailableProcessors());
-        EdictContext.sender().sendMessage(C.GOLD + "CPU Load: " + Form.pc(Platform.CPU.getCPULoad()) + " CPU Live Process Load: " + Form.pc(Platform.CPU.getLiveProcessCPULoad()));
+        EdictContext.sender().sendMessage(C.AQUA + "- CPU Architecture: " + Platform.CPU.getArchitecture() + " Available Processors: " + Platform.CPU.getAvailableProcessors());
+        EdictContext.sender().sendMessage(C.AQUA + "- CPU Load: " + Form.pc(Platform.CPU.getCPULoad()) + " CPU Live Process Load: " + Form.pc(Platform.CPU.getLiveProcessCPULoad()));
+
         CommandSender s = EdictContext.sender();
         J.a(() -> Hastebin.enviornment(s));
     }
